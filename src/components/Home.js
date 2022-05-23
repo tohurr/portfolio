3a2e2400626f992ec386bae11e03a4c95bab3560
Tohur Rahman <tohurrahman@Tohurs-MBP.home>
2022-05-23 19:14:56 -0400

    Added two new components and used React router to create my web page

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+   return(
+        <div>
+            <h1>
+            Welcome! Thank you for landing on my page 😎
+            </h1>
+        </div>
+    )
+}
+
+export default Home

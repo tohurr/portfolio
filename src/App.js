e8b985640bff7d4be32299a1450fa6cfc830e07d
Tohur Rahman <tohurrahman@Tohurs-MBP.home>
2022-05-24 01:06:17 -0400

    Installed react-redux

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
       <Routes >
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-
         <Route path="*" element={<NotFound/>} />
       </Routes>
      </BrowserRouter> 
